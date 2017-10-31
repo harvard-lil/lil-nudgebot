862af28ea1276cdd0746f7769952b7c5d1decc07
Ben Steinberg <bsteinberg@law.harvard.edu>
2017-10-31 11:23:25 -0400

    Generalize channels, reorg for PEP8

--- bin/bot.py
+++ bin/bot.py
@@ -20,9 +20,13 @@ logging.basicConfig(format='%(levelname)s:%(message)s',
 # only run on weekdays
 if datetime.today().weekday() in range(5):
     slack = Slacker(os.environ.get('SLACK_API_TOKEN'))
-    urls = os.environ.get('NUDGE_PULLS_URL')
-    for url in urls.split(os.environ.get('NUDGE_PULLS_URL_SPLIT_ON')):
-        r = requests.get(os.environ.get('NUDGE_PULLS_URL'))
+    targets = os.environ.get('NUDGE_PULLS_URL_CHANNEL')
+    # this is awkward, and requires a well-formed env var: 'url1#channel1|url2#channel2' etc.,
+    # where NUDGE_PULLS_URL_SPLIT_ON is '|'
+    for (url, channel) in [(target[0], '#{}'.format(target[1]))
+                           for target in map(lambda x: x.split('#'),
+                                             targets.split(os.environ.get('NUDGE_PULLS_URL_SPLIT_ON')))]:
+        r = requests.get(url)
         jsoned_response = json.loads(r.text)
 
         now = datetime.now(pytz.utc)
@@ -32,7 +36,7 @@ if datetime.today().weekday() in range(5):
         for pull_req in jsoned_response:
             requested_at = parser.parse(pull_req['created_at'])
             time_diff = now - requested_at
-            hours_since_pull_req = int(time_diff.total_seconds() /60/60)
+            hours_since_pull_req = int(time_diff.total_seconds() / 60 / 60)
 
             emoji = False
             if hours_since_pull_req in range(12, 36):
@@ -50,12 +54,12 @@ if datetime.today().weekday() in range(5):
             # Pass the custom accept header to requests, because this API is preview mode
             # https://developer.github.com/v3/pulls/reviews/
             reviews_url = pull_req['url'] + '/reviews'
-            reviews = json.loads(requests.get(reviews_url, headers={'accept': 'application/vnd.github.black-cat-preview+json'}).text)
+            reviews = json.loads(requests.get(reviews_url,
+                                              headers={'accept': 'application/vnd.github.black-cat-preview+json'}).text)
 
             if emoji and not any(review['state'] == 'CHANGES_REQUESTED' for review in reviews):
                 nudged = True
                 user = pull_req['head']['user']['login']
-                channel = os.environ.get('NUDGE_CHANNEL')
 
                 # Get combined status for this pull req.
                 # To do this we convert the `statuses_url` GitHub gave us from something like
@@ -69,9 +73,11 @@ if datetime.today().weekday() in range(5):
                 # If tests have failed, send a message to the channel -- we can't send to just the user,
                 # since github username and slack username don't necessarily match.
                 if status_summary['state'] == 'failure':
-                    message = "Uh oh -- tests have failed on %s's %s %s" % (pull_req['head']['user']['login'], pull_req['html_url'], emoji)
+                    message = "Uh oh -- tests have failed on %s's %s %s" % (pull_req['head']['user']['login'],
+                                                                            pull_req['html_url'], emoji)
                 else:
-                    message = "Don't keep %s waiting %s %s" % (pull_req['head']['user']['login'], emoji, pull_req['html_url'])
+                    message = "Don't keep %s waiting %s %s" % (pull_req['head']['user']['login'], emoji,
+                                                               pull_req['html_url'])
 
                 slack.chat.post_message(channel, message, as_user=True)
                 logging.info("Nudged about %s" % (pull_req['html_url'],))
